e7390283c6a4a7ae1278811d2c0ba5c885a45f2a
Jonathan Borg <jonathan@x3m.io>
2019-12-17 23:34:02 +0100

    fix: add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,24 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-154722259-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "x3m.io",
+      },
+    {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
